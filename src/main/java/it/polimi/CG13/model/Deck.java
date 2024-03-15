4464b93be337f4063be8470b2939da894c58f7dc
Pietro <pietro.venturelli@mail.polimi.it>
2024-03-15 16:10:58 +0100

    sistemato problema di deck che aveva perso le librerie

--- src/main/java/it/polimi/CG13/model/Deck.java
+++ src/main/java/it/polimi/CG13/model/Deck.java
@@ -1,5 +1,11 @@
 package it.polimi.CG13.model;
 
+//import of the library of Google
+import java.util.*;
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Deck {
     ArrayList<ObjectiveCard> objectiveDeck;
     ArrayList<PlayableCard> resourceDeck;
