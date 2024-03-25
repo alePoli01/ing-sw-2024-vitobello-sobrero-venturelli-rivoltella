7fb283f154db934660f07e73724c856c28e6a381
NICCOLO <niccolo.sobrero@mail.polimi.it>
2024-03-25 17:52:54 +0100

    Modifiche fatte: 1) creato file JSON per i mazzi oro e risorsa 2) creato metodo parseJSON per salvare i deck nelle variabili resourceDeck e goldDeck  3) creato DeckTest

--- src/main/java/it/polimi/CG13/model/Deck.java
+++ src/main/java/it/polimi/CG13/model/Deck.java
@@ -1,6 +1,12 @@
 package it.polimi.CG13.model;
 
 //import of the library of Google
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.util.*;
 
 public class Deck {
@@ -8,6 +14,13 @@ public class Deck {
     LinkedList<PlayableCard> startDeck;
     LinkedList<PlayableCard> resourceDeck;
     LinkedList<PlayableCard> goldDeck;
+
+    public Deck() {
+        this.objectiveDeck = null;
+        this.startDeck = null;
+        this.resourceDeck = null;
+        this.goldDeck = null;
+    }
 
     public LinkedList<PlayableCard> getResourceDeck() {
         return resourceDeck;
@@ -31,4 +44,31 @@ public class Deck {
         Collections.shuffle(resourceDeck);
         Collections.shuffle(goldDeck);
     }
+
+    public void parseJSON(){
+        Gson gson = new Gson();
+
+        //lettura carte risorsa (
+        try {
+            FileReader reader = new FileReader("src/main/resources/Decks.json");
+
+            // Definisci il tipo per deserializzare
+            Type type = new TypeToken<Map<String, LinkedList<PlayableCard>>>(){}.getType();
+            Map<String, LinkedList<PlayableCard>> map = gson.fromJson(reader, type);
+
+            this.resourceDeck = map.get("resourceDeck");
+            this.goldDeck = map.get("goldDeck");
+            this.startDeck = map.get("startDeck");
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
+
+
--- src/test/java/it/polimi/CG13/model/DeckTest.java
+++ src/test/java/it/polimi/CG13/model/DeckTest.java
@@ -1,7 +1,108 @@
 package it.polimi.CG13.model;
 
+import it.polimi.CG13.enums.CardType;
+import it.polimi.CG13.enums.ReignType;
 import junit.framework.TestCase;
 
 public class DeckTest extends TestCase {
 
+    public void testParseJSON() {
+        Deck deck = new Deck();
+
+        deck.parseJSON();
+
+        assertNotNull(deck.resourceDeck);
+        assertNotNull(deck.goldDeck);
+        assertNotNull(deck.startDeck);
+        //assertNotNull(deck.objectiveDeck);
+
+
+        for(PlayableCard card : deck.resourceDeck){
+            System.out.println("Serial Number: " + card.getSerialNumber());
+            System.out.println("Card Type: " + card.getCardType());
+            System.out.println("Reign: " + card.getReign());
+            System.out.println("Points: " + card.getPointsGiven());
+
+            if(card.getCardType().equals(CardType.GOLD)) {
+                for (ReignType reign : ReignType.values()) {
+                    System.out.println("Resource Needed: "+ card.getResourceNeeded(reign));
+                }
+            }
+
+            System.out.println("edge available");
+            for(int i=0; i<4; i++){
+                System.out.println(card.edgeAvailable(i));
+            }
+            System.out.println("reign edge");
+            for(int i=0; i<4; i++){
+                System.out.println(card.getReignPointEdge(i));
+            }
+            System.out.println("object point edge");
+            for(int i=0; i<4; i++){
+                System.out.println(card.getObjectPointEdge(i));
+            }
+
+
+            System.out.println("-----------------------");
+        }
+
+        for(PlayableCard card : deck.goldDeck){
+            System.out.println("Serial Number: " + card.getSerialNumber());
+            System.out.println("Card Type: " + card.getCardType());
+            System.out.println("Reign: " + card.getReign());
+            System.out.println("Points: " + card.getPointsGiven());
+
+            if(card.getCardType().equals(CardType.GOLD)) {
+                for (ReignType reign : ReignType.values()) {
+                    System.out.println("Resource Needed: "+ card.getResourceNeeded(reign));
+                }
+            }
+
+            System.out.println("edge available");
+            for(int i=0; i<4; i++){
+                System.out.println(card.edgeAvailable(i));
+            }
+            System.out.println("reign edge");
+            for(int i=0; i<4; i++){
+                System.out.println(card.getReignPointEdge(i));
+            }
+            System.out.println("object point edge");
+            for(int i=0; i<4; i++){
+                System.out.println(card.getObjectPointEdge(i));
+            }
+
+
+            System.out.println("-----------------------");
+        }
+
+        for(PlayableCard card : deck.startDeck){
+            System.out.println("Serial Number: " + card.getSerialNumber());
+            System.out.println("Card Type: " + card.getCardType());
+            System.out.println("Reign: " + card.getReign());
+            System.out.println("Points: " + card.getPointsGiven());
+
+            if(card.getCardType().equals(CardType.GOLD)) {
+                for (ReignType reign : ReignType.values()) {
+                    System.out.println("Resource Needed: "+ card.getResourceNeeded(reign));
+                }
+            }
+
+            System.out.println("edge available");
+            for(int i=0; i<4; i++){
+                System.out.println(card.edgeAvailable(i));
+            }
+            System.out.println("reign edge");
+            for(int i=0; i<4; i++){
+                System.out.println(card.getReignPointEdge(i));
+            }
+            System.out.println("object point edge");
+            for(int i=0; i<4; i++){
+                System.out.println(card.getObjectPointEdge(i));
+            }
+
+
+            System.out.println("-----------------------");
+        }
+
+    }
 }
