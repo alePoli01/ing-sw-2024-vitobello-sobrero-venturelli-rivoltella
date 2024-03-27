b9e02c7ac5e9425cb14fdd162b42853a8510fedd
RiccardoRivo <riccardo.rivoltella@mail.polimi.it>
2024-03-27 16:50:23 +0100

    Modificato ForbiddenCoordinatesException Creato test per classe Coordinates
    Creati test per classe Game (incompleti)

--- src/main/java/it/polimi/GC13/exception/ForbiddenCoordinatesException.java
+++ src/main/java/it/polimi/GC13/exception/ForbiddenCoordinatesException.java
@@ -3,6 +3,6 @@ package it.polimi.GC13.exception;
 public class ForbiddenCoordinatesException extends Exception{
 
     public ForbiddenCoordinatesException(int x, int y) {
-        super(x + y + "are not valid coordinates");
+        super("("+x+"," +y+ ") are not valid coordinates");
     }
 }
--- src/test/java/it/polimi/GC13/model/CoordinatesTest.java
+++ src/test/java/it/polimi/GC13/model/CoordinatesTest.java
@@ -1,9 +1,39 @@
 package it.polimi.GC13.model;
 
+import it.polimi.GC13.exception.ForbiddenCoordinatesException;
 import junit.framework.TestCase;
 
 public class CoordinatesTest extends TestCase {
+    public void testEvenVerifierOnZero() {
+        //test if EvenVerifier accepts (0,0)
+        int x=0;
+        int y=0;
+        var coordinates= new Coordinates(x,y);
 
-    public void testEvenVerifier() {
+        try {
+            coordinates.evenVerifier();
+        }catch(ForbiddenCoordinatesException fc){
+            fail("coordinates: ("+x+","+y+") should not throw exception");
+        }
     }
+    public void testEvenVerifierOnNegatives(){
+        //test if EvenVerifier can accept negative values (-1,-4)F and (-1,-3)T
+        int x=-1;
+        int y=-4;
+        var coordinates= new Coordinates(x,y);
+        try {
+            coordinates.evenVerifier();
+            fail("coordinates: ("+x+","+y+") should throw exception");
+        }catch(ForbiddenCoordinatesException fc){
+            assertEquals("("+x+"," +y+ ") are not valid coordinates",fc.getMessage());
+        }
+        y=-3;
+        coordinates.setY(y);
+        try {
+            coordinates.evenVerifier();
+        }catch(ForbiddenCoordinatesException fc){
+            fail("coordinates: ("+x+","+y+") should not throw exception");
+        }
+    }
+
 }
