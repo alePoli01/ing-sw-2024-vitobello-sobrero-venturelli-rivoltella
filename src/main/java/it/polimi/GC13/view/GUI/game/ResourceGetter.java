7ae0d6604f5497ac44f93a1808c41ec33a1fc2a7
alePoli01 <alessandro.vitobello@mail.polimi.it>
2024-06-28 11:18:27 +0200

    resources

--- src/main/java/it/polimi/GC13/view/GUI/game/ResourceGetter.java
+++ src/main/java/it/polimi/GC13/view/GUI/game/ResourceGetter.java
@@ -4,19 +4,8 @@ import it.polimi.GC13.view.GUI.Getter;
 
 import java.net.URL;
 
-/**
- * The {@code ResourceGetter} class implements the {@link Getter} interface.
- * It provides methods to retrieve resource URLs based on resource names.
- */
 public class ResourceGetter implements Getter {
 
-    /**
-     * Retrieves the URL of the specified resource.
-     *
-     * @param resource The name or path of the resource
-     * @return The URL object representing the resource location,
-     *         or {@code null} if the resource is not found
-     */
     @Override
     public URL getURL(String resource) {
         return getClass().getResource(resource);
